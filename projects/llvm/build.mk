69a2e527b820700c23a8a44b6fe0b7a76bb7d1f1
feicong <fei_cong@hotmail.com>
2025-11-30 18:55:20 +0800

    Bump llvm version from 18.1.8 to 20.1.8

--- projects/llvm/build.mk
+++ projects/llvm/build.mk
@@ -77,7 +77,7 @@ $(LLVM_HOST_BUILD_DIR):
 		$(LLVM_EXTRA_CMAKE_FLAGS) \
 		$(LLVM_EXTRA_HOST_FLAGS)
 
-LLVM_BRANCH_OR_TAG = llvmorg-18.1.8
+LLVM_BRANCH_OR_TAG = llvmorg-20.1.8
 LLVM_REPO = https://github.com/llvm/llvm-project
 projects/llvm/sources:
 	git clone $(LLVM_REPO) $@ --depth=1 -b $(LLVM_BRANCH_OR_TAG)
